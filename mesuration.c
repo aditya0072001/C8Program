06ef04511165c709a06a3ac102a0fc5af3b30f96
Ne <41997646+aditya0072001@users.noreply.github.com>
2019-01-21 18:11:46 +0530

    Add files via upload

---
+++ mesuration.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+int arearectangle(int l,int b)
+{
+    return l*b;
+}
+
+int perimeterrectangle(int l,int b)
+{
+    return 2+(l*b);
+}
+
+int areasquare(int s)
+{
+    return s*s;
+}
+
+
+int perimetersquare(int s)
+{
+    return 4*s;
+}
+
+int arearhombus(int d1,int d2)
+{
+    return 0.5*d1*d2;
+}
+
+int perimeterrhombus(int s)
+{
+    return 4*s;
+}
