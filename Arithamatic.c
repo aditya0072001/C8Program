b1d7376d3a8eb2e07ce06b88642e179de9439bac
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-01-23 19:57:21 +0530

    Arithematic

---
+++ Arithamatic.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<math.h>
+int hcftwo(int n1,int n2)
+{
+     
+
+}
+
+int hcfthree(int n1,int n2,int n3)
+{
+    
+
+}
+
+int lcmtwo(int n1,int n2)
+{
+    
+
+}
+
+int lcmthree(int n1,int n2,int n3)
+{
+    
+
+}
+
+int sum(int a,int b)
+{
+    return a+b;
+}
+
+int substract(int a,int b)
+{
+    return a-b;
+}
+
+int multiply(int a,int b)
+{
+    return a*b;
+}
+
+float divison(float a,float b)
+{
+    return a/b;
+}
+
