77c066385ae719cdc976d8a30defe153ecc049f8
aditya0072001 <tripathi.aditya1660@gmail.com>
2019-01-26 20:27:29 +0530

    sin cos tan working now

--- trigometry.h
+++ trigometry.h
@@ -2,25 +2,27 @@
 #include<math.h>
 double sin(double degree)
 {
- double answer;
-    answer=sin(degree);
+ double answer,angle;
+    angle=degree *(3.142/180.0);
+    answer=sin(angle);
     return answer;
 }
 
 double cos(double degree)
 {
-    double answer;
-
-    answer=cos(degree);
+    double answer,angle;
+    angle=degree *(3.142/180.0);
+    answer=cos(angle);
 
     return answer;
 }
 
 double tan(double degree)
 {
-   double answer;
-
-    answer=tan(degree);
+   double answer,angle;
+    angle=degree *(3.142/180.0);
+    answer=tan(angle);
+    
 
     return answer;
 }
